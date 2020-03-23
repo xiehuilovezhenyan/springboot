d9e4bc6c29d563cfeec267b2ced1dec9809f5906
xiehuilovezhenyan <xiehuilovezhenyan@163.com>
2020-03-23 22:02:59 +0800

    1

---
+++ springboot-gateway/src/main/java/com/xiehui/StartApplication.java
@@ -0,0 +1,23 @@
+package com.xiehui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * springboot启动类
+ * 
+ * @author xiehui
+ *
+ */
+@EnableDiscoveryClient 
+@SpringBootApplication 
+@EnableFeignClients 
+public class StartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartApplication.class, args);
+	}
+
+}
---
+++ springboot-gateway/src/main/resources/readme.txt
@@ -0,0 +1,5 @@
+说明:
+本项目是为了自学一些实用技能知识自建项目
+
+
+1: 网关gateway  https://www.cnblogs.com/cloudxlr/p/11795040.html
