42ebc646a767d8fa6934001c82a60d4a821452e1
xiehuilovezhenyan <xiehuilovezhenyan@163.com>
2020-05-14 19:13:37 +0800

    1

--- springboot-a/springboot-webapp/springboot-webapp-wechat/src/main/java/com/xiehui/controller/ESController.java
+++ springboot-a/springboot-webapp/springboot-webapp-wechat/src/main/java/com/xiehui/controller/ESController.java
@@ -6,37 +6,37 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.xiehui.common.core.annotation.Access;
-import com.xiehui.es.TestServiceImpl;
+import com.xiehui.es.ElasticsearchServiceImpl;
 import com.xiehui.es.UserInfo;
 
 @RestController
 @RequestMapping("/api/v1/es")
 public class ESController {
     @Autowired
-    private TestServiceImpl testService;
+    private ElasticsearchServiceImpl esService;
 
     @RequestMapping("/es/{id}")
     @Access(isLogin = false, isSign = false, isTimestamp = false)
     public UserInfo queryAccountInfo(@PathVariable("id") Long id) {
-        return testService.queryUserInfoById(id);
+        return esService.queryUserInfoById(id);
     }
 
     @RequestMapping("/es/query/{userName}")
     @Access(isLogin = false, isSign = false, isTimestamp = false)
     public UserInfo queryAccountInfoByAccountName(@PathVariable("userName") String userName) {
-        return testService.queryUserInfoByUserName(userName);
+        return esService.queryUserInfoByUserName(userName);
     }
 
     @RequestMapping("/es/save/{id}")
     @Access(isLogin = false, isSign = false, isTimestamp = false)
     public void save(@PathVariable("id") String id) {
-        testService.save(id);
+        esService.save(id);
     }
     
     @RequestMapping("/es/reloadClickLog")
     @Access(isLogin = false, isSign = false, isTimestamp = false)
     public void reloadClickLog() {
-        testService.reloadClickLog();
+        esService.reloadClickLog();
     }
 
 }
